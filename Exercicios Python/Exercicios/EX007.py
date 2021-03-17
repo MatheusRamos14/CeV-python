6e3f4ba8418de5ae72d0499b73a808a3ae44eb58
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2021-03-17 09:20:17 -0300

    Exercícios 7-9

---
+++ Exercicios Python/Exercicios/EX007.py
@@ -0,0 +1,4 @@
+B1 = int(input('Primeira nota:'))
+B2 = int(input('Segunda nota:'))
+M = (B1+B2)/2
+print('\033[34mA média deste aluno é de {}'.format(M))
---
+++ Exercicios Python/Exercicios/EX008.py
@@ -0,0 +1,4 @@
+M = float(input('Digite um valor de metro:'))
+C = M*100
+MM = M*1000
+print('\033[33m{} contém {}cm e {}mm.'.format(M, C, MM))
