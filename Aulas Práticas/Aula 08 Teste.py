3b55f05e396f8f0a5d24469196f0b05d257c708e
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2021-03-20 09:26:30 -0300

    Aula 08

---
+++ Aulas Práticas/Aula 08 Teste.py
@@ -0,0 +1,5 @@
+from math import sqrt,floor,ceil
+N=int(input('Digite um numero:'))
+R=sqrt(N)
+##print('A raiz quadrada de {} é igual a {}'.format(N,math.ceil(R)))
+#print('A raiz quadrada de {} é igual a {}'.format(N,math.floor(R)))
